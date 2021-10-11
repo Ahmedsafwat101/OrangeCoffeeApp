1d330f7522974df88797de5b21b2c3e24b4cbd62
Safwatzzz <42112466+Ahmedsafwat101@users.noreply.github.com>
2021-10-12 01:58:15 +0200

    Admission View Model Input Validation

--- app/src/main/java/com/orangecoffeeapp/ui/useradmission/AdmissionViewModel.kt
+++ app/src/main/java/com/orangecoffeeapp/ui/useradmission/AdmissionViewModel.kt
@@ -5,10 +5,14 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.orangecoffeeapp.R
+import com.orangecoffeeapp.constants.ErrorMessage
+import com.orangecoffeeapp.constants.ErrorMessage.ERROR_EMAIL_IS_EMPTY
 import com.orangecoffeeapp.data.models.LoginFormModel
 import com.orangecoffeeapp.data.models.UserModel
 import com.orangecoffeeapp.data.repository.AdmissionRepository
 import com.orangecoffeeapp.utils.admission.AdmissionState
+import com.orangecoffeeapp.utils.admission.LoginFormUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -25,22 +29,34 @@ constructor(private val repo: AdmissionRepository)
     private  val TAG = "MyViewModel"
     private val user = MutableLiveData<AdmissionState<UserModel>>()
 
+
     fun getUser() =  user
 
     fun logIn(data:Any){
         user.postValue(AdmissionState.Loading)
         val currUser = data as LoginFormModel
-        viewModelScope.launch(Dispatchers.IO) {
-            val response =  repo.validateUserLogin(currUser)
-            if(response!=null) {
-                Log.d(TAG, "Debug " + response.lastName)
-                if (response.password == currUser.password) {
-                    user.postValue(AdmissionState.Success(response))
+        if(validateFields(currUser.email,currUser.password)) {
+            viewModelScope.launch(Dispatchers.IO) {
+                val response = repo.validateUserLogin(currUser)
+                if (response != null) {
+                    Log.d(TAG, "Debug " + response.lastName)
+                    if (response.password == currUser.password) {
+                        user.postValue(AdmissionState.Success(response))
+                    }
+                } else {
+                    user.postValue(AdmissionState.Error("Network Error"))
                 }
-            }else{
-                user.postValue(AdmissionState.Error("Errror"))
             }
         }
+    }
+
+    private fun validateFields(email: String, password: String): Boolean {
+        val result = LoginFormUtils.validateLoginForm(email,password)
+        if (result != ErrorMessage.NONE) {
+            user.postValue(AdmissionState.Error(result))
+            return false
+        }
+        return true
     }
 
 }
--- app/src/main/java/com/orangecoffeeapp/ui/useradmission/LogInFragment.kt
+++ app/src/main/java/com/orangecoffeeapp/ui/useradmission/LogInFragment.kt
@@ -67,51 +67,25 @@ class LogInFragment : Fragment() {
         //Check
         logInBinding.logInBtn.setOnClickListener {
             // If valid input return NONE
-            it?.apply { isEnabled = false; postDelayed({isEnabled=true},400) }
-            Log.d(TAG,"else before"+isLogged().toString())
-            if (setInputError() == NONE) {
+            it?.apply { isEnabled = false; postDelayed({ isEnabled = true }, 400) }
+            Log.d(TAG, "else before" + isLogged().toString())
 
-                admissionViewModel.viewModelScope.launch {
-                    admissionViewModel.logIn(
-                        LoginFormModel(
-                            logInBinding.logInEmailTxt.text.toString().trim(),
-                            logInBinding.loginInPasswordTxt.text.toString().trim()
-                        )
+            admissionViewModel.viewModelScope.launch {
+                admissionViewModel.logIn(
+                    LoginFormModel(
+                        logInBinding.logInEmailTxt.text.toString().trim(),
+                        logInBinding.loginInPasswordTxt.text.toString().trim()
                     )
-                }
+                )
             }
         }
     }
 
-    private fun setInputError(): String {
-        val result = LoginFormUtils.validateLoginForm(
-            logInBinding.logInEmailTxt.text.toString(),
-            logInBinding.loginInPasswordTxt.text.toString()
-        )
-        if (result != NONE) {
+
+    private fun subscribeObserver() {
+        admissionViewModel.getUser().observe(viewLifecycleOwner, { result ->
             when (result) {
-                ERROR_EMAIL_IS_EMPTY -> {
-                    logInBinding.logInEmailTxt.error = result
-                }
-                ERROR_EMAIL_NOT_VALID_MSG -> {
-                    logInBinding.logInEmailTxt.error = result
-                }
-                ERROR_PASSWORD_IS_EMPTY -> {
-                    logInBinding.loginInPasswordTxt.error = result
-                }
-                ERROR_PASSWORD_LENGTH_LESS_THAN_8 -> {
-                    logInBinding.loginInPasswordTxt.error = result
-                }
-            }
-            displaySnackbar(result)
-        }
-        return result
-    }
-
-    private fun subscribeObserver(){
-        admissionViewModel.getUser().observe(viewLifecycleOwner,{ result ->
-            when(result){
-                is AdmissionState.Success->{
+                is AdmissionState.Success -> {
                     displayProgressbar(false)
                     //save in SharedPreference
                     saveSharedPreferenceData(result.data)
@@ -122,12 +96,29 @@ class LogInFragment : Fragment() {
                     }
 
                 }
-                is AdmissionState.Loading->{
+                is AdmissionState.Loading -> {
                     displayProgressbar(true)
                 }
-                is AdmissionState.Error->{
+                is AdmissionState.Error -> {
+                    result
                     displayProgressbar(false)
+                    when (result.e) {
+                        ERROR_EMAIL_IS_EMPTY -> {
+                            logInBinding.logInEmailTxt.error = result.e
+                        }
+                        ERROR_EMAIL_NOT_VALID_MSG -> {
+                            logInBinding.logInEmailTxt.error = result.e
+                        }
+                        ERROR_PASSWORD_IS_EMPTY -> {
+                            logInBinding.loginInPasswordTxt.error = result.e
+                        }
+                        ERROR_PASSWORD_LENGTH_LESS_THAN_8 -> {
+                            logInBinding.loginInPasswordTxt.error = result.e
+                        }
+                    }
+                    displaySnackbar(result.e)
                     Log.d("here", "Error ${result.e}")
+
                     displaySnackbar(result.e)
                 }
             }
@@ -148,52 +139,56 @@ class LogInFragment : Fragment() {
     }
 
     private fun isLogged(): Boolean {
-        val loginSharedPref = requireActivity().getSharedPreferences("loginRPref", Context.MODE_PRIVATE)
-        return  loginSharedPref.getBoolean("logged", false)
+        val loginSharedPref =
+            requireActivity().getSharedPreferences("loginRPref", Context.MODE_PRIVATE)
+        return loginSharedPref.getBoolean("logged", false)
     }
 
-    private fun saveSharedPreferenceData(data: UserModel){
-        var loginSharedPref = requireActivity().getSharedPreferences("loginRPref", Context.MODE_PRIVATE)
+    private fun saveSharedPreferenceData(data: UserModel) {
+        var loginSharedPref =
+            requireActivity().getSharedPreferences("loginRPref", Context.MODE_PRIVATE)
         val editor = loginSharedPref.edit()
         editor.apply {
-            putString("fName",data.firstName)
-            putString("lName",data.lastName)
-            putString("email",data.email)
-            putString("password",data.password)
-            putString("phone",data.phone)
-            putBoolean("access",data.access)
-            putString("type",data.type)
-            putBoolean("logged",true)
+            putString("fName", data.firstName)
+            putString("lName", data.lastName)
+            putString("email", data.email)
+            putString("password", data.password)
+            putString("phone", data.phone)
+            putBoolean("access", data.access)
+            putString("type", data.type)
+            putBoolean("logged", true)
         }.apply()
     }
 
 
     private fun getSharedPreferenceData(): UserModel {
-        val loginSharedPref = requireActivity().getSharedPreferences("loginRPref", Context.MODE_PRIVATE)
+        val loginSharedPref =
+            requireActivity().getSharedPreferences("loginRPref", Context.MODE_PRIVATE)
 
         return UserModel(
             firstName = loginSharedPref?.getString("fName", null).toString(),
             lastName = loginSharedPref?.getString("lName", null).toString(),
             email = loginSharedPref?.getString("email", null).toString(),
             phone = loginSharedPref?.getString("phone", null).toString(),
-            access = loginSharedPref.getBoolean("access",false),
+            access = loginSharedPref.getBoolean("access", false),
             type = loginSharedPref?.getString("type", null).toString(),
             password = loginSharedPref?.getString("password", null).toString(),
         )
     }
 
 
-    private fun navigateToHomeScreen(type:String){
-        Log.d(TAG,"else navigateToHomeScreen"+type)
+    private fun navigateToHomeScreen(type: String) {
+        Log.d(TAG, "else navigateToHomeScreen" + type)
         when (type) {
-            UserTypes.Admin ->{
-                Log.d(TAG,"else navigateToHomeScreen in in"+type)
+            UserTypes.Admin -> {
+                Log.d(TAG, "else navigateToHomeScreen in in" + type)
 
-                findNavController().navigate(R.id.action_logInFragment_to_adminHomeFragment)}
+                findNavController().navigate(R.id.action_logInFragment_to_adminHomeFragment)
+            }
             UserTypes.Customer -> findNavController().navigate(R.id.action_logInFragment_to_customerHomeFragment)
             UserTypes.Owner -> findNavController().navigate(R.id.action_logInFragment_to_ownerHomeFragment)
-            else->{
-                Log.d(TAG,"else nav"+type)
+            else -> {
+                Log.d(TAG, "else nav" + type)
 
             }
         }
